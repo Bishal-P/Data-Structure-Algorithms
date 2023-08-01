2d84f83ad532ef69a8193d594be6063b38e6fc71
Bishal P <120594033+Bishal-P@users.noreply.github.com>
2023-08-01 23:19:10 +0530

    Create 05. sum_of_digits.cpp

---
+++ 05. sum_of_digits.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    int sum=0,product=1;
+    while(n>0){
+        int rem=n%10;
+        sum+=rem;
+        product*=rem;
+        n/=10;
+
+    }
+    cout<<"The difference of product and sum is :"<<(product-sum)<<endl;
+}

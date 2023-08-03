4c9bd80b4b85f1e14101b0523abd732317243ac1
Bishal P <120594033+Bishal-P@users.noreply.github.com>
2023-08-03 23:15:57 +0530

    Update 02.binary_to_decimal.cpp

--- 02.binary_to_decimal.cpp
+++ 02.binary_to_decimal.cpp
@@ -1,17 +1,12 @@
 #include<iostream>
 #include<string>
 #include<math.h>
-
 using namespace std;
-
 int main(){
-
     char binary[]="11111";
     int len=binary.length(),integer=0;
     for(int i=0;i<len;i++){
         char num=binary[1];
-        // int t=stoi(num);
-        // integer+=pow(2,num);
         cout<<num;
     }
     // cout<<integer;

6cfc3c91f31805d78d011ee12a472e9fdab9f54f
Bishal <bishalprasad251122@gmail.com>
2023-08-15 05:28:22 +0530

    decimal to binary

---
+++ loops/DecimalToBinary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int num=5;
+    int binary=0;
+    while(num>0){
+        int rem=num&1;
+        binary=rem*pow(10,i)+binary;
+        num=num/2;
+    }
+    cout<<"The binay number is "<<binary<<endl;
+}

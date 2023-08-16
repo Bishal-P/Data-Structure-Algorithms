83aab75266673d3d035c85923888a2b1c9c47c93
Bishal <bishalprasad251122@gmail.com>
2023-08-16 20:18:20 +0530

    reverse of the number

---
+++ 07.reverseOfNumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter the number :";
+    cin >> num;
+    int i = 0, rev = 0;
+    while (num > 0)
+    {
+        int rem = num % 10;
+        rev = rev * 10 + rem;
+        num /= 10;
+    }
+    cout << "The reverse of the number is :" << rev << endl;
+}

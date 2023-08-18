6a4274099c6a96d938f6202213c961fec4b3ba7d
Bishal <bishalprasad251122@gmail.com>
2023-08-18 20:00:43 +0530

    unique element

---
+++ 16.findDuplicate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int arr[7]={1,4,3,5,2,6,4};
+    int ans=0;
+    for(int i=0;i<7;i++){
+        ans^=arr[i];
+    }
+    for(int i=1;i<7;i++){
+        ans^=i;
+    }
+    cout<<"The unique elements is :"<<ans;
+}

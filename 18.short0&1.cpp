ea3a1bca540504b07ed752a67f6222d9ba140d14
Bishal <bishalprasad251122@gmail.com>
2023-08-19 22:07:03 +0530

    having errors

--- 18.short0&1.cpp
+++ 18.short0&1.cpp
@@ -8,15 +8,27 @@ int main(){
     int arr[8]={1,0,0,0,1,1,0,0};
     int start=0,end=7,temp;
     while(start<end){
-        if(arr[start]>arr[end]){
+        if(arr[start]=arr[end]){
+            while(start<end){
+                if(arr[start]<arr[end]){
+                    // start++;
+                    break;
+                }
+                start++;
+            }
             temp=arr[start];
             arr[start]=arr[end];
             arr[end]=temp;
         }
-        start++;
-        end--;
+        else{
+            temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
     }
     for(int i=0;i<8;i++){
-        cout<<i;
+        cout<<arr[i];
     }
 }

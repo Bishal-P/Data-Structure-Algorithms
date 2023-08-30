18bc09b2ae6dffab4cb8497b0c88818b788f098a
Bishal P <bishalprasad251122@gmail.com>
2023-08-30 14:48:48 +0530

    not done contain errors

---
+++ 20.bound.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int leftBound(int arr[],int size,int key){
+    int start=0,end=size-1;
+    int ans=-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==key){
+            ans=mid;
+            end=mid-1;
+        }
+        if(arr[mid]>key){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return ans;
+}
+
+
+int rightBound(int arr[],int size,int key){
+    int start=0,end=size-1;
+    int ans=-1;
+    while(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==key){
+            ans=mid;
+            end=mid+1;
+        }
+        if(arr[mid]>key){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    int arr[13]={1,2,3,3,3,3,3,3,3,5,6,7,9};
+    cout<<"The start index is :"<<leftBound(arr,13,3)<<endl;
+    cout<<"The start index is :"<<rightBound(arr,13,3)<<endl;
+}
+
